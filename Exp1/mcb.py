3f88857cc2a0207df44db577259bc2143dbab015
Hatim Sawai <hatimsawai76@gmail.com>
2023-09-06 10:00:29 +0530

    exp1 a
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ Exp1/mcb.py
@@ -0,0 +1,40 @@
+def main():
+    print("Welcome to Missionary and Cannibals Game!\nRules:\nNo. of Missionaries cannot be less than no. of cannibals at any point on any bank!\n\nLet's Begin!")
+    lm = int(input("Enter the number of missionaries: "))
+    lc = int(input("Enter the number of cannibals: "))
+    b = rm = rc = 0
+    print(f"Initial State(on left):\nMissionaries: {lm}\nCannibals: {lc}\nBoat is on left Bank!\n")
+    flag = True
+    while(flag):
+        if lm == 0 and lc == 0:
+            print("You Won the Game!\n")
+            print(f"Final State on Left:\nMissionaries: {lm}\nCannibals: {lc}\n")
+            print(f"Final State on Right:\nMissionaries: {rm}\nCannibals: {rc}\n")
+            flag = False
+            break
+        um, uc = map(int, input("Enter the no. of missionaries and cannibals: ").split())
+        if b == 0:
+            lm -= um
+            lc -= uc
+            rm += um
+            rc += uc
+        else:
+            lm += um
+            lc += uc
+            rm -= um
+            rc -= uc
+        if (rm < rc and rm != 0) or (lm < lc and lm!= 0):
+            print("Missionaries are less than cannibals!\nYou Lost the Game!")
+            flag = False
+            break
+        else:
+            print(f"Current State on Left Bank:\nMissionaries: {lm}\nCannibals: {lc}\n")
+            print(f"Current State on Right Bank:\nMissionaries: {rm}\nCannibals: {rc}\n")
+            if b == 0:
+                b = 1
+            elif b == 1:
+                b = 0
+            print("Boat is on left Bank!\n") if b == 0 else print("Boat is on right Bank!\n")
+    
+if __name__ == "__main__":
+    main()
